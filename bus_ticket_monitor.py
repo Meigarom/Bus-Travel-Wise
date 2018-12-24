9c039c99a724f4822049080f3168423409d61bbe
meigarom <meigaromlopes@gmail.com>
2018-12-24 14:30:09 -0200

    finish script

--- bus_ticket_monitor.py
+++ bus_ticket_monitor.py
@@ -9,44 +9,55 @@ import json
 import pandas        as pd
 import requests     
 
+# generate range of date to consult
+date_range = pd.date_range(pd.datetime.now(), periods=32).strftime( '%Y-%m-%d' ).tolist()
+
 # parameters
 root = 'https://www.clickbus.com.br/'
 vehicle = 'onibus'
 origin = 'sao-paulo-sp-todos'
-destination = 'mogi-guacu-sp'
-date = '2018-12-25'
+destinations = ['campinas-sp', 'mogi-guacu-sp']
 
-# website address
-web_address = root+vehicle+'/'+origin +'/'+destination+'?departureDate='+date 
+df_final = pd.DataFrame()
+for destination in destinations:
 
-print( web_address )
+    for date in date_range:
+        # website address
+        web_address = root+vehicle+'/'+origin +'/'+destination+'?departureDate='+date 
 
-# access the link
-response = requests.get( web_address )
-page = response.text
-soup = BeautifulSoup( page,"lxml" )
+        print( web_address )
 
-# -------------------------------------------
-# get bus lines information & seats available
-# -------------------------------------------
-seats = soup.find_all( class_='available-seats' ) 
-buslines = soup.find_all( class_="search-item search-item-direct " )
-data_list = list()
-for busline, seat in zip( buslines, seats ):
-    data = json.loads( busline['data-content'] )['trips'][0]
-    data_dict = { 'departureTime': data['departureTime'], 
-                  'company': data['companySlug'], 
-                  'arrivalDate': data['arrivalDate'], 
-                  'arrivalStation': data['arrivalStation'], 
-                  'departureStation': data['departureStation'],
-                  'arrivalTime': data['arrivalTime'],
-                  'departureDate': data['departureDate'], 
-                  'durationTime': data['durationTime'], 
-                  'seats_available': int( re.findall( r'\d+', seat.get_text() )[0] ), 
-                  'timestampScrape': datetime.now().strftime( '%Y-%m-%dT%H:%M:%S' ) }
-    data_list.append( data_dict )
+        # access the link
+        response = requests.get( web_address )
+        page = response.text
+        soup = BeautifulSoup( page,"lxml" )
 
-df = pd.DataFrame( data_list )
-print( df )
+        # get bus lines information & seats available
+        date_now = datetime.now().strftime( '%Y-%m-%dT%H:%M:%S' )
+        seats = soup.find_all( class_='available-seats' ) 
+        buslines = soup.find_all( class_="search-item search-item-direct " )
+
+        # iterate over bus lines information and seats available
+        data_list = list()
+        for busline, seat in zip( buslines, seats ):
+            data = json.loads( busline['data-content'] )['trips'][0]
+            data_dict = { 'departureTime': data['departureTime'], 
+                          'company': data['companySlug'], 
+                          'arrivalDate': data['arrivalDate'], 
+                          'arrivalStation': data['arrivalStation'], 
+                          'departureStation': data['departureStation'],
+                          'arrivalTime': data['arrivalTime'],
+                          'departureDate': data['departureDate'], 
+                          'durationTime': data['durationTime'], 
+                          'seats_available': int( re.findall( r'\d+', seat.get_text() )[0] ), 
+                          'price': data['price'],
+                          'timestampScrape': date_now }
+            data_list.append( data_dict )
+
+        df = pd.DataFrame( data_list, columns=['departureDate', 'arrivalDate', 'departureStation', 'arrivalStation', 'departureTime', 'arrivalTime', 
+                                               'durationTime', 'company', 'price', 'seats_available', 'timestampScrape'] )
+
+    df_final = pd.concat( [df_final, df] )
 
 # save the dataset as a csv file
+df_final.to_csv( 'buslines-at-{}.csv'.format( re.sub( r':', '-', date_now ) ) )
