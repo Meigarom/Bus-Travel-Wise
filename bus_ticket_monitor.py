1dcd387abb784afe4615dfe67103da3c1791d4ec
meigarom <meigaromlopes@gmail.com>
2018-12-24 13:28:02 -0200

    finish script to get bus ticket from one day

--- bus_ticket_monitor.py
+++ bus_ticket_monitor.py
@@ -1,16 +1,13 @@
 # -*- coding: utf-8 -*-
 
 # libraries
+from bs4            import BeautifulSoup 
+from datetime       import datetime
+
 import re
 import json
-import time
-import requests
-import pandas                       as pd
-from selenium import webdriver
-from bs4      import BeautifulSoup 
-
-## Chrome driver
-#driver = webdriver.Chrome( executable_path='/Users/meigarom/chromedriver' )
+import pandas        as pd
+import requests     
 
 # parameters
 root = 'https://www.clickbus.com.br/'
@@ -25,35 +22,17 @@ web_address = root+vehicle+'/'+origin +'/'+destination+'?departureDate='+date
 print( web_address )
 
 # access the link
-#driver.get( web_address )
-#
-## ----------------------------
-## get to buy bus ticket
-## ----------------------------
-#time.sleep( 3 )
-#btn = driver.find_element_by_xpath( '//*[@id="search-results"]/div[2]/div/div[1]/div[1]/div[7]/button[1]' )
-#btn.click()
-#
-#time.sleep( 2 )
-
 response = requests.get( web_address )
 page = response.text
 soup = BeautifulSoup( page,"lxml" )
 
-# ------------------------
-# get all seats available
-# ------------------------
+# -------------------------------------------
+# get bus lines information & seats available
+# -------------------------------------------
 seats = soup.find_all( class_='available-seats' ) 
-
-df_seats = list()
-for seat in seats:
-    df_seats.append( int( re.findall( r'\d+', seat.get_text() )[0] )  )
-
-# ------------------------
-# get bus lines information
-# ------------------------
 buslines = soup.find_all( class_="search-item search-item-direct " )
-for busline in buslines:
+data_list = list()
+for busline, seat in zip( buslines, seats ):
     data = json.loads( busline['data-content'] )['trips'][0]
     data_dict = { 'departureTime': data['departureTime'], 
                   'company': data['companySlug'], 
@@ -62,34 +41,12 @@ for busline in buslines:
                   'departureStation': data['departureStation'],
                   'arrivalTime': data['arrivalTime'],
                   'departureDate': data['departureDate'], 
-                  'durationTime': data['durationTime'] }
-    print( data_dict )
+                  'durationTime': data['durationTime'], 
+                  'seats_available': int( re.findall( r'\d+', seat.get_text() )[0] ), 
+                  'timestampScrape': datetime.now().strftime( '%Y-%m-%dT%H:%M:%S' ) }
+    data_list.append( data_dict )
 
-
-## create dataframe
-#df = pd.DataFrame( [ {'departureTime': data['trips'][0]['departureTime'], 
-#                     'company': data['trips'][0]['companySlug'], 
-#                     'arrivalDate': data['trips'][0]['arrivalDate'], 
-#                     'arrivalStation': data['trips'][0]['arrivalStation'], 
-#                     'departureStation': data['trips'][0]['departureStation'],
-#                     'arrivalTime': data['trips'][0]['arrivalTime'],
-#                     'departureDate': data['trips'][0]['departureDate'], 
-#                     'durationTime': data['trips'][0]['durationTime'] } ] )
-#
-#print( df )
-#
-# Convert to dataframe
-
-
-
-#print( len( soup.find_all( class_='available-seats' ) ) )
-#print( soup.find_all( class_='available-seats' )[0].get_text() )
-
-
-#print( soup )
-#for tag in soup.find_all( 'title' ):
-#    print( tag.text )
-
-# mount the dataset
+df = pd.DataFrame( data_list )
+print( df )
 
 # save the dataset as a csv file
